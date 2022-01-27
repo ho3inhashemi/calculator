89934556221d138c25720cfc645a0d12fb7317cd
ho3inhashemi <ho3inhashemi@gmail.com>
2022-01-27 14:25:08 +0330

    constraint added

--- routes/web.php
+++ routes/web.php
@@ -9,11 +9,11 @@ Route::get('/', function () {
 
 Route::get('/add/{a}/{b}', function ($a,$b) {
     return $a+$b;
-});
+})->where(['a' => '[0-9]+', 'b' => '[0-9]+']);
 
 Route::get('/minus/{a}/{b}', function ($a,$b) {
     return $a-$b;
-});
+})->where(['a' => '[0-9]+', 'b' => '[0-9]+']);
 
 
 
